f8e60eaa59860ebd881beeb6e2169062b7bb2b62
Matkuchen <mat.kuchen@gmail.com>
2026-05-20 18:50:18 -0300

    Padronização
    nivel -> level

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,14 +3,14 @@ import 'enums.dart';
 class UserModel {
   final String id;
   final String nome;
-  final UserLevels nivel;
+  final UserLevels level;
   final int points;
   final UserRoles role;
 
   UserModel({
     required this.id,
     required this.nome,
-    this.nivel = UserLevels.junior,
+    this.level = UserLevels.junior,
     this.points = 0,
     this.role = UserRoles.student,
   });
@@ -19,7 +19,7 @@ class UserModel {
     return {
       'id': id,
       'nome': nome,
-      'nivel': nivel.name,
+      'level': level.name,
       'points': points,
       'role': role.name,
     };
@@ -29,7 +29,7 @@ class UserModel {
     return UserModel(
       id: map['id'] ?? '',
       nome: map['nome'] ?? '',
-      nivel: UserLevels.values.firstWhere(
+      level: UserLevels.values.firstWhere(
             (e) => e.name == map['nivel'],
         orElse: () => UserLevels.junior,
       ),
